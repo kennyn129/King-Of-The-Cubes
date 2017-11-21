c0d0fce9949ea5c4126d39c02acdcc9a0d6b23b8
Wesley Pickering <wesley.pickering@berkeley.edu>
2017-11-20 17:05:12 -0800

    added new scenes

---
+++ proj3TEST/Assets/Scripts/UI/ChooseMap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseMap : MonoBehaviour {
+
+	public void LoadMainMenu () {
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void LoadMap1 () {
+		SceneManager.LoadScene ("Map1");
+	}
+
+	public void LoadMap2 () {
+		SceneManager.LoadScene ("Map2");
+	}
+
+	public void LoadMap3 () {
+		SceneManager.LoadScene ("Map3");
+	}
+
+	public void LoadMap4 () {
+		SceneManager.LoadScene ("Map4");
+	}
+
+	public void LoadMap5 () {
+		SceneManager.LoadScene ("Map5");
+	}
+
+	public void LoadMap6 () {
+		SceneManager.LoadScene ("Map6");
+	}
+}
---
+++ proj3TEST/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void LoadPlay() {
+		SceneManager.LoadScene ("ChooseMap");
+	}
+
+	public void LoadControls() {
+		SceneManager.LoadScene ("Controls");
+	}
+
+	public void LoadSettings() {
+		SceneManager.LoadScene ("Settings");
+	}
+
+	public void LoadCredits() {
+		SceneManager.LoadScene ("Credits");
+	}
+}
